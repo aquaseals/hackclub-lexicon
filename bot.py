0d2d13e63bf1fac04e93ae7149e1aad7a87dda3a
aquaseals <itzme.haya@gmail.com>
2025-05-04 16:39:00 -0400

    added message handling

--- bot.py
+++ bot.py
@@ -17,5 +17,15 @@ slack_event_adapter = SlackEventAdapter(
 )
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
+client.chat_postMessage(channel='#lexicon-testing', text="hello world!")
+
+@slack_event_adapter.on('message')
+def message (payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
